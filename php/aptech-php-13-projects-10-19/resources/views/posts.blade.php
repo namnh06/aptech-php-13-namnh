3b261eb2ec0ba71f02a0fe529dd65eed87a7b23c
[name] <namnh.website@gmail.com>
2018-08-15 21:22:13 +0700

    updated session 11 - solved 10-19

---
+++ php/aptech-php-13-projects-10-19/resources/views/posts.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+
+<body>
+  <h1 class="text-danger">Hello World</h1>
+  {{dd($posts)}}
+
+  <script src="{{asset('js/bootstrap.js')}}"></script>
+</body>
+
+</html>
